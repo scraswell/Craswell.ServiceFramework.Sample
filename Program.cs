639bca203d413573e053ed620a291b2388292383
Sean Craswell <scraswell@gmail.com>
2016-07-08 22:17:44 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Craswell.ServiceBase;
+
+namespace Craswell.ServiceFramework.Sample
+{
+    /// <summary>
+    /// The sample service program.
+    /// </summary>
+    internal class Program
+        : ServiceWrapper<Host<Service>, Service>
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        private static void Main(string[] args)
+        {
+            StartService(args);
+        }
+    }
+}
